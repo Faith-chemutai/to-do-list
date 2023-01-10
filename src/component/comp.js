67c38dc467ae9ca77d52290037e1f10fa2dc14a1
Faith <faithchemutai2330@gmail.com>
2023-01-10 21:41:28 +0300

    completed assignment

---
+++ src/component/comp.js
@@ -0,0 +1,19 @@
+
+
+export function toDoList(){
+    return(
+        <h3>to do list</h3>
+    )
+}
+export function toDoItem(){
+    return(
+    <ol>
+<li>Do assignment</li>
+<li>Pack items</li>
+<li>Do laundry</li>
+<li>pay fees</li>
+
+    </ol>
+
+    );
+}
